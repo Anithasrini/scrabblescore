0b587b8271806b65f8647f717e664aca0e4139ee
Anitha Srini and Chris Siems <student@epicodus.com>
2016-01-05 12:01:25 -0800

    create a way to get letter value from user input.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,23 @@
 import static spark.Spark.*;
+import java.util.Arrays;
 
 public class App {
   public static void main(String[] args) {}
 
-    public String scrabbleScore(String userInput) {
-        for ( Integer i = 0; i < userInput.length(); i++ ) {
-          char[] scrabbleCharArray = userInput.toCharArray();
-          char result = scrabbleCharArray[i];
-          String convertedChar = Character.toString(result);
-          return convertedChar;
-        }
-        return "That is not a valid word.";
-        }
+    public Integer scrabbleScore(String userInput) {
+
+      String[] scrabbleLetters = {"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"};
+      Integer[] scrabbleValues = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+      Integer scrabbleScore = 0;
+
+      for ( Integer i = 0; i < userInput.length(); i++ ) {
+        char[] scrabbleCharArray = userInput.toCharArray();
+        char result = scrabbleCharArray[i];
+        String convertedChar = Character.toString(result);
+        Integer letterIndex = Arrays.asList(scrabbleLetters).indexOf(convertedChar);
+        Integer letterValue = scrabbleValues[letterIndex];
+        scrabbleScore += letterValue;
+      }
+      return scrabbleScore;
+      }
     }
-    // String[] scrabbleLetters = {A};
-    // Integer[] scrabbleValues = {1};
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,11 +3,25 @@ import static org.junit.Assert.*;
 
 public class AppTest {
   @Test
-  public void scrabbleScore_returnsAScrabbleScoreForALetter_A() {
+  public void scrabbleScore_returnsAScrabbleScoreForALetter_1() {
     App app = new App();
-    String score = "a";
+    Integer score = 1;
     assertEquals(score, app.scrabbleScore("a"));
   }
+  @Test
+  public void scrabbleScore_returnIndexOfUserInputInScrabbleLetterArray_1() {
+    App app = new App();
+    Integer index = 1;
+    assertEquals(index, app.scrabbleScore("a"));
+  }
+  @Test
+  public void scrabbleScore_returnsAScrabbleScoreForMultipleLetters_3() {
+    App app = new App();
+    Integer score = 3;
+    assertEquals(score, app.scrabbleScore("aaa"));
+  }
+
+
 }
 
 
